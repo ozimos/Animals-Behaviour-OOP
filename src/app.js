ce5c93a1f2f9ebb36914365d5f2a3c2099536bea
Tovieye Ozi <tovieye.ozi@gmail.com>
2017-11-21 13:31:16 +0100

    Add some classes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-export default () => 'hello'
-;
+export default class Animal {
+  constructor(name, callNumber, speed) {
+    this.name = name;
+    this.callNumber = callNumber;
+    this.speed = speed;
+  }
+
+  sayHi() {
+    alert(this.name);
+  }
+  speak() {
+    return `A string`;
+  }
+  move() {
+    return `A call`;
+  }
+}
+class Mammal extends Animal {
+  constructor(name, callNumber, speed) {
+    super(name, callNumber, speed);
+  }
+}
+class Birds extends Animal {
+  constructor(name, callNumber, speed) {
+    super(name, callNumber, speed);
+  }
+}
--- test/appTest.js
+++ test/appTest.js
@@ -1,14 +1,16 @@
 import chai from 'chai';
 import app from '../src/app';
 
-const assert = chai.assert;
+const {
+  assert
+} = chai.assert;
 
 describe('app to test as', () => {
   const result = app();
-  it('app should return hello', () => {
+  it('app should return a class', () => {
     assert.equal(result, 'hello');
   }); // we put app() becos we checking for app()----goto terminal type npm run test
-
+  nimal = new app.Animal();
   it('it should be a string', () => {
     assert.typeOf(result, 'string');
   });
